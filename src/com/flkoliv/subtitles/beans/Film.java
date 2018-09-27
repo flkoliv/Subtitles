e0be2584480387b14306d6cf9f022dad5f73d2e4
flkoliv <olivier@fleck.xyz>
2018-09-27 21:29:59 +0200

    fin mise en place DAO film

--- src/com/flkoliv/subtitles/beans/Film.java
+++ src/com/flkoliv/subtitles/beans/Film.java
@@ -1,5 +1,41 @@
 package com.flkoliv.subtitles.beans;
 
 public class Film {
-
+	
+	private int id;
+	private String nom;
+	private String langue;
+	private String nomFichier;
+	
+	public Film() {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getLangue() {
+		return langue;
+	}
+	public void setLangue(String langue) {
+		this.langue = langue;
+	}
+	public String getNomFichier() {
+		return nomFichier;
+	}
+	public void setNomFichier(String nomFichier) {
+		this.nomFichier = nomFichier;
+	}
+	
+	
+	
 }
--- src/com/flkoliv/subtitles/dao/FilmDaoImpl.java
+++ src/com/flkoliv/subtitles/dao/FilmDaoImpl.java
@@ -1,25 +1,61 @@
 package com.flkoliv.subtitles.dao;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.flkoliv.subtitles.beans.Film;
 
 public class FilmDaoImpl implements FilmDao {
 
-	public FilmDaoImpl(DaoFactory daoFactory) {
-		// TODO Auto-generated constructor stub
-	}
+	private DaoFactory daoFactory;
+	
+	FilmDaoImpl(DaoFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
 
 	@Override
-	public void ajouter(Film utilisateur) {
-		// TODO Auto-generated method stub
-		
+	public void ajouter(Film film) {
+		Connection connexion = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connexion = daoFactory.getConnection();
+            preparedStatement = connexion.prepareStatement("INSERT INTO films(nom, langue, fichier) VALUES(?, ?, ?);");
+            preparedStatement.setString(1, film.getNom());
+            preparedStatement.setString(2, film.getLangue());
+            preparedStatement.setString(2, film.getNomFichier());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 	}
 
 	@Override
 	public List<Film> lister() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		List<Film> films = new ArrayList<Film>();
+        Connection connexion = null;
+        Statement statement = null;
+        ResultSet resultat = null;
 
+        try {
+            connexion = daoFactory.getConnection();
+            statement = connexion.createStatement();
+            resultat = statement.executeQuery("SELECT id, nom, langue, fichier FROM films;");
+
+            while (resultat.next()) {
+                Film film = new Film();
+                film.setId(resultat.getInt("id"));
+                film.setNom(resultat.getString("nom"));
+                film.setLangue(resultat.getString("langue"));
+                film.setLangue(resultat.getString("fichier"));
+                films.add(film);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return films;
+    }
 }
+
+
